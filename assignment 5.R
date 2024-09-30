40143fe9919ea45efeb90f815ad533a533eb20fb
SAINATH9030224 <guddetir@usf.edu>
2024-09-29 21:52:24 -0400

    Add files via upload

---
+++ assignment 5.R
@@ -0,0 +1,20 @@
+A <- matrix(1:100, nrow=10)  
+B <- matrix(1:1000, nrow=10)
+
+
+# matrix for A
+A
+# matrix for B
+B
+
+# determinant matrix for A
+det(A)
+
+# determinant matrix for b
+det(B)
+
+#inverse matrix for A
+solve(A)
+
+# inverse matrix for B
+solve(B)
